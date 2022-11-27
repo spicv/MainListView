fe05b6f8b39be68a1686bf23ea5d7164294498a1
Student <spicv>
2022-11-27 14:22:22 +0200

    commit

--- app/src/main/java/com/example/mainlistview/SimonActivity.java
+++ app/src/main/java/com/example/mainlistview/SimonActivity.java
@@ -2,6 +2,9 @@ package com.example.mainlistview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -12,24 +15,49 @@ import android.widget.GridLayout;
 import android.widget.LinearLayout;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 public class SimonActivity extends AppCompatActivity implements View.OnClickListener, Animation.AnimationListener {
+    RunIt playAnim;
+    Animation animationFadeOut;
     GridLayout gl;
     Button btnStart;
+//    SoundPool soundPool;
     Button[][]buttons=new Button[3][3];
     ArrayList<Button> buttonOrder = new ArrayList<>();
-    RunIt playAnim;
+    int[] colors={Color.parseColor("#e3342f"),Color.parseColor("#f6993f"),Color.parseColor("#ffed4a"),Color.parseColor("#38c172"),Color.parseColor("#4dc0b5"),Color.parseColor("#3490dc"),Color.parseColor("#6574cd"),Color.parseColor("#9561e2"),Color.parseColor("#f66d9b"),};
+//    HashMap<Button,Integer>btnSounds;
 
-    Animation animationFadeOut;
-
+    //int b1, b2, b3, b4, b5, b6, b7, b8, b9;
     int order=0;
+    int colorIndex=0;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simon);
+
+//        AudioAttributes aa = new AudioAttributes.Builder()
+//                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+//                .setUsage(AudioAttributes.USAGE_MEDIA)
+//                .build();
+//        soundPool = new SoundPool.Builder()
+//                .setMaxStreams(10)
+//                .setAudioAttributes(aa)
+//                .build();
+//        b1 = soundPool.load(this, R.raw.c3, 1);
+//        b2 = soundPool.load(this, R.raw.doo, 1);
+//        b3 = soundPool.load(this, R.raw.fa, 1);
+//        b4 = soundPool.load(this, R.raw.g6, 1);
+//        b5 = soundPool.load(this, R.raw.la, 1);
+//        b6 = soundPool.load(this, R.raw.mi, 1);
+//        b7 = soundPool.load(this, R.raw.re, 1);
+//        b8 = soundPool.load(this, R.raw.si, 1);
+//        b9 = soundPool.load(this, R.raw.sol, 1);
+//        int[]sounds={b1,b2,b3,b4,b5,b6,b7,b8,b9};
+
         gl=findViewById(R.id.gl);
         playAnim = new RunIt();
         btnStart=findViewById(R.id.btnStart);
@@ -37,6 +65,13 @@ public class SimonActivity extends AppCompatActivity implements View.OnClickList
         animationFadeOut= AnimationUtils.loadAnimation(SimonActivity.this, R.anim.fade_out_simon);
         animationFadeOut.setAnimationListener(this);
         createBoard();
+
+        for (int i = 0; i < buttons.length; i++) {
+            for (int j = 0; j < buttons[i].length; j++) {
+ //               btnSounds.put(buttons[i][j],sounds[i]);
+            }
+        }
+
 
     }
 
@@ -80,6 +115,7 @@ public class SimonActivity extends AppCompatActivity implements View.OnClickList
             new Thread(playAnim).start();
         }
         if (!buttonText.equalsIgnoreCase("start game")&&b.equals(buttonOrder.get(order))){
+//            soundPool.play(btnSounds.get(0), 1, 1, 0, 0, 1);
             order++;
             if (order==buttonOrder.size()){
                 addRandomBtn();
@@ -108,14 +144,16 @@ public class SimonActivity extends AppCompatActivity implements View.OnClickList
         for (int i = 0; i < buttons.length; i++) {
             for (int j = 0; j < buttons[i].length; j++) {
                 LinearLayout.LayoutParams LL1=new LinearLayout.LayoutParams(330,330);
-                LL1.setMargins(10,10,10,10);
+                LL1.setMargins(15,15,15,15);
                 Button btn=new Button(this);
                 btn.setLayoutParams(LL1);
                 btn.setTextSize(40);
                 btn.setOnClickListener(this);
+                btn.setBackgroundColor(colors[colorIndex]);
                 buttons[i][j]=btn;
                 gl.addView(btn);
                 gl.setVisibility(View.INVISIBLE);
+                colorIndex++;
             }
         }
     }
