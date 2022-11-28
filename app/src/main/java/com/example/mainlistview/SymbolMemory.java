b1909166ca64c058bfffad28e86235074ffd346f
Student <spicv>
2022-11-28 15:01:13 +0200

    commit

---
+++ app/src/main/java/com/example/mainlistview/SymbolMemory.java
@@ -0,0 +1,14 @@
+package com.example.mainlistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SymbolMemory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_symbol_memory);
+    }
+}
